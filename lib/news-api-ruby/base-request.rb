a412f7c8ea5a7565bb480ce3f2e7bec6ea08c315
vvendemia <vvendemia1@gmail.com>
2017-02-27 20:23:01 -0500

    Refactoring of BaseRequest

--- lib/news-api-ruby/base-request.rb
+++ lib/news-api-ruby/base-request.rb
@@ -1,40 +1,36 @@
 
 
 module NewsApi
-
   class BaseRequest
+    class << self
 
-    BASE_REQUEST_URL = 'https://newsapi.org/v1'
-    SUCCESS 		 = 200
+      BASE_REQUEST_URL = 'https://newsapi.org/v1'
+      SUCCESS 		 = 200
 
-    attr_reader :api_key
-    def initialize(api_key)
-      @api_key = api_key
-    end
-
-    def get endpoint
-      response = Faraday.get "#{BASE_REQUEST_URL}/#{endpoint}"
-      if is_success?(response)
-        parse response.body
-      else
-        raise failed_request response
+      def get endpoint
+        response = Faraday.get "#{BASE_REQUEST_URL}/#{endpoint}"
+        if is_success?(response)
+          parse response.body
+        else
+          raise failed_request response
+        end
       end
+
+      private
+
+      def parse response
+        JSON.parse response
+      end
+
+      def is_success? response
+        response.status == SUCCESS
+      end
+
+      def failed_request response
+        msg = "#{response.status}: #{response.reason_phrase}"
+        NewsApi::Exceptions::FailedRequest.new msg
+      end
+
     end
-
-    private
-
-    def parse response
-      JSON.parse response
-    end
-
-    def is_success? response
-      response.status == SUCCESS
-    end
-
-    def failed_request response
-      msg = "#{response.status}: #{response.reason_phrase}"
-      NewsApi::Exceptions::FailedRequest.new msg
-    end
-
   end
 end
