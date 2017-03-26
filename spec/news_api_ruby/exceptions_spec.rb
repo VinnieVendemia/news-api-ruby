05fda00edd20f29219db82a7fec91228000e1b82
vvendemia <vvendemia1@gmail.com>
2017-03-26 16:45:26 -0400

    Add Exceptions spec

---
+++ spec/news_api_ruby/exceptions_spec.rb
@@ -0,0 +1,17 @@
+require 'news_api_ruby'
+
+# Tests for Exceptions
+describe NewsApi::Exceptions do
+
+  context '#initialize' do
+
+    let(:msg) do
+      "400: Bad Request"
+    end
+
+    it 'should be possible to initialize a FailedRequest' do
+      err = NewsApi::Exceptions::FailedRequest.new msg
+      expect(err).to be_truthy
+    end
+  end
+end
