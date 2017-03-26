32af58f23ba9cd95db88b831e13f8c7764fd7863
vvendemia <vvendemia1@gmail.com>
2017-03-26 15:13:57 -0400

    Remove initialize block from Source

--- lib/news_api_ruby/source.rb
+++ lib/news_api_ruby/source.rb
@@ -4,13 +4,6 @@ module NewsApi
   # A Source object contains read-only attributes for currently available
   # news sources on https://newsapi.org/
   class Source < BaseRequest
-    def initialize source
-      source.each do |k, v|
-        instance_variable_set("@#{k}", v)
-        singleton_class.class_eval { attr_reader k }
-      end
-    end
-
     class << self
       def all
         get('sources')['sources'].map { |s| new s }
