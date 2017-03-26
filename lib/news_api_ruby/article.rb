d8e76c17cded83b48519b4857d0956054db7b131
vvendemia <vvendemia1@gmail.com>
2017-03-26 15:27:39 -0400

    Update article to handle string parameter or Source object for method all

--- lib/news_api_ruby/article.rb
+++ lib/news_api_ruby/article.rb
@@ -3,15 +3,23 @@ module NewsApi
   # An Article object contains read-only attributes for a list of currently
   # available news Articles from a particular source
   class Article < BaseRequest
-
-  	API_KEY_PARAM = "apiKey=#{ENV['API_KEY']}"
+    API_KEY_PARAM = "apiKey=#{ENV['API_KEY']}".freeze
 
     class << self
-      # Retrieve a list of current news headlines from a given source 
-      # returns [Array] array of Article objects 
+      # Retrieve a list of current news headlines from a given source
+      # returns [Array] array of Article objects
       def all source
-        endpoint = "articles?source=#{source.id}&#{API_KEY_PARAM}"
+        source = source_id(source)
+        endpoint = "articles?source=#{source}&#{API_KEY_PARAM}"
         get(endpoint)['articles'].map { |a| new a }
+      end
+
+      private
+
+      # determines if parameter is already a source id string or
+      # if a source object was passed, return a valid source id
+      def source_id source
+        source.is_a?(NewsApi::Source) ? source.id : source
       end
     end
   end
